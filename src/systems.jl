1660262ae4aeb8d4608e8b079f20246468add02b
Addison Schile <addschile@gmail.com>
2021-08-08 17:00:47 -0700

    moved abstractions away from here, added time, removed force and extra memory from here

--- src/systems.jl
+++ src/systems.jl
@@ -1,28 +1,63 @@
-abstract type AbstractSystem end
-
 mutable struct DummySystem <: AbstractSystem; end
 
-mutable struct ThermostattedSystem <: AbstractSystem
-  model::AbstractPotential
-  thermostat::AbstractThermostat
-  x::Array{Float64}
-  f::Array{Float64}
-  em::AbstractArray
-  function ThermostattedSystem(model::AbstractPotential,thermostat::AbstractThermostat)
-    dim::Int64 = getdimensionality(model)
-    x::Array{Float64} = zeros(dim)
-    f::Array{Float64} = zeros(dim)
-    em::AbstractArray = genextramem(model)
-    new(model,thermostat,x,f,em)
+mutable struct System <: AbstractSystem
+  dim::Int64
+  t::Float64
+  x::Vector{Float64}
+  f::Vector{Float64}
+  function System(dim::Int64,t::Float64,x::Vector{Float64},f::Vector{Float64})
+    new(dim,t,x,f)
   end
 end
 
-function fderivative(du::AbstractArray,u::AbstractArray,sys::AbstractSystem)
-  du .*= 0.0
-  force!(u,du,sys.em,sys.model)
+function System(model::AbstractPotential)
+  dim::Int64 = getdimensionality(model)
+  x::Vector{Float64} = zeros(dim)
+  f::Vector{Float64} = zeros(dim)
+  System(dim,0.0,x,f)
 end
 
-function gderivative(du::AbstractArray,u::AbstractArray,therm::AbstractThermostat)
-  du .*= 0.0
-  diffusion!(du,therm)
+mutable struct ThermostattedSystem <: AbstractSystem
+  dim::Int64
+  t::Float64
+  x::Vector{Float64}
+  f::Vector{Float64}
+  thermostat::AbstractThermostat
+  function ThermostattedSystem(dim::Int64,t::Float64,x::Vector{Float64},f::Vector{Float64},thermostat::AbstractThermostat)
+    new(dim,t,x,f,thermostat)
+  end
 end
+
+function ThermostattedSystem(model::AbstractPotential,thermostat::AbstractThermostat)
+  dim::Int64 = getdimensionality(model)
+  x::Vector{Float64} = zeros(dim)
+  f::Vector{Float64} = zeros(dim)
+  ThermostattedSystem(dim,0.0,x,f,thermostat)
+end
+
+function zeroforce!(system::AbstractSystem)
+  system.f .= 0.0
+end
+
+#function fderivative(du::AbstractArray,u::AbstractArray,sys::AbstractSystem)
+#  force!(u,du,sys.em,sys.model)
+#end
+#
+#function gderivative(du::AbstractArray,u::AbstractArray,therm::AbstractThermostat)
+#  diffusion!(du,therm)
+#end
+#
+#mutable struct OverdampedSystem <: AbstractSystem
+#  model::AbstractPotential
+#  thermostat::AbstractThermostat
+#  x::Array{Float64}
+#  f::Array{Float64}
+#  em::AbstractArray
+#  function OverdampedSystem(model::AbstractPotential,thermostat::AbstractThermostat)
+#    dim::Int64 = getdimensionality(model)
+#    x::Array{Float64} = zeros(dim)
+#    f::Array{Float64} = zeros(dim)
+#    em::AbstractArray = genextramem(model)
+#    new(model,thermostat,x,f,em)
+#  end
+#end
