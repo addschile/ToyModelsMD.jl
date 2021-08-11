c835a56e6d5089304974e9ba06ed0730a0906912
Addison Schile <addschile@gmail.com>
2021-08-11 09:37:45 -0700

    commented out addforce

--- src/potentials.jl
+++ src/potentials.jl
@@ -1,5 +1,5 @@
 struct DummyPotential <: AbstractPotential; end
 
-function addforce!(dt::Float64,system::AbstractSystem,model::AbstractPotential)
-  system.x .+= dt .* model.f
-end
+#function addforce!(dt::Float64,system::AbstractSystem,model::AbstractPotential)
+#  system.x .+= dt .* model.f
+#end
