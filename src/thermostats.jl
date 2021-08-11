28306efcd9976ed804978f9d14380d3f097e6124
Addison Schile <addschile@gmail.com>
2021-08-11 09:39:36 -0700

    added active langevin thermostat for rotational diffusion

--- src/thermostats.jl
+++ src/thermostats.jl
@@ -1,6 +1,6 @@
 struct DummyThermostat <: AbstractThermostat; end
 
-mutable struct Langevin <: AbstractThermostat
+mutable struct Langevin <: AbstractLangevin
   T::Float64
   gamma::Float64
   scale::Float64
@@ -12,18 +12,42 @@ mutable struct Langevin <: AbstractThermostat
 end
 Langevin(T::Float64,gamma::Float64) = Langevin(T,gamma,rand(UInt64))
 
+"""
+"""
+mutable struct ActiveLangevin <: AbstractLangevin
+  Tt::Float64
+  gammat::Float64
+  Tr::Float64
+  gammar::Float64
+  scale::Vector{Float64}
+  rng::AbstractRNG
+  rands::Vector{Float64}
+  function ActiveLangevin(dim::Int64,Tt::Float64,gammat::Float64,Tr::Float64,gammar::Float64,seed::UInt64)
+    scale = ones(Float64,dim+1)
+    scale[1:dim] .= sqrt(2.0*Tt/gammat)
+    scale[end] = sqrt(2.0*Tr/gammar)
+    new(Tt,gammat,Tr,gammar,scale,MersenneTwister(seed),[])
+  end
+end
+ActiveLangevin(dim::Int64,Tt::Float64,gammat::Float64,Tr::Float64,gammar::Float64) = ActiveLangevin(dim,Tt,gammat,Tr,gammar,rand(UInt64))
+
+"""
+"""
 function gennoises(nsteps::Int64,dim::Int64,rng::AbstractRNG)
   randn(rng,Float64,(nsteps,dim))
 end
 
-function gennoises!(nsteps::Int64,dim::Int64,langevin::Langevin)
+function gennoises!(nsteps::Int64,dim::Int64,langevin::AbstractLangevin)
   langevin.rands = randn(langevin.rng,Float64,(nsteps,dim))
 end
 
-function gennoise!(dim::Int64,langevin::Langevin)
+function gennoise!(dim::Int64,langevin::AbstractLangevin)
   langevin.rands = randn(langevin.rng,Float64,dim)
+  #langevin.rands = ones(Float64,dim)
 end
 
+"""
+"""
 function diffusion!(du::Array{Float64},langevin::Langevin)
   du .= 1.0
 end
