74eadb1c0bff13c40cb44ca7b3f55addca4ea6a0
Addison Schile <addschile@gmail.com>
2021-08-11 09:39:06 -0700

    added active brownian system, moved addforce to this function to this file for dispatch-ability

--- src/systems.jl
+++ src/systems.jl
@@ -1,4 +1,4 @@
-mutable struct DummySystem <: AbstractSystem; end
+struct DummySystem <: AbstractSystem; end
 
 mutable struct System <: AbstractSystem
   dim::Int64
@@ -17,7 +17,9 @@ function System(model::AbstractPotential)
   System(dim,0.0,x,f)
 end
 
-mutable struct ThermostattedSystem <: AbstractSystem
+"""
+"""
+mutable struct ThermostattedSystem <: AbstractThermostattedSystem
   dim::Int64
   t::Float64
   x::Vector{Float64}
@@ -37,6 +39,35 @@ end
 
 function zeroforce!(system::AbstractSystem)
   system.f .= 0.0
+end
+
+function addforce!(dt::Float64,system::AbstractSystem,model::AbstractPotential)
+  system.x .+= dt .* model.f
+end
+
+"""
+"""
+mutable struct ActiveBrownianSystem <: AbstractThermostattedSystem
+  dim::Int64
+  v0::Float64
+  t::Float64
+  x::Vector{Float64}
+  f::Vector{Float64}
+  thermostat::ActiveLangevin
+  function ActiveBrownianSystem(dim::Int64,v0::Float64,t::Float64,x::Vector{Float64},f::Vector{Float64},thermostat::ActiveLangevin)
+    new(dim,v0,t,x,f,thermostat)
+  end
+end
+
+function ActiveBrownianSystem(v0::Float64,model::AbstractPotential,thermostat::ActiveLangevin)
+  dim::Int64 = getdimensionality(model)
+  x::Vector{Float64} = zeros(dim+1)
+  f::Vector{Float64} = zeros(dim)
+  ActiveBrownianSystem(dim+1,v0,0.0,x,f,thermostat)
+end
+
+function addforce!(dt::Float64,sys::ActiveBrownianSystem,model::AbstractPotential)
+  @. @views sys.x[1:sys.dim-1] .+= dt .* (model.f + (sys.v0 .* [cos(sys.x[end]),sin(sys.x[end])]))
 end
 
 #function fderivative(du::AbstractArray,u::AbstractArray,sys::AbstractSystem)
