6b9d2cc1c37577947e0238a0937da6028a41f917
Addison Schile <addschile@gmail.com>
2021-08-11 09:36:39 -0700

    added abstract types for thermostatted systems and abstractlangevin type

--- src/abstracttypes.jl
+++ src/abstracttypes.jl
@@ -4,9 +4,11 @@ abstract type AbstractMixedModel <: AbstractPotential end
 
 # systems
 abstract type AbstractSystem end
+abstract type AbstractThermostattedSystem <: AbstractSystem end
 
 # thermostats
 abstract type AbstractThermostat end
+abstract type AbstractLangevin <: AbstractThermostat end
 
 # integrators
 abstract type AbstractIntegrator end
