90e50b4a0a17b432b61ef7ade85ff7bffd4d61a9
Addison Schile <addschile@gmail.com>
2021-08-16 11:52:08 -0700

    cleaned up code by taking out comments

--- src/mcvb.jl
+++ src/mcvb.jl
@@ -62,15 +62,11 @@ function callback(cb::MCVBCallback,system::AbstractSystem,mm::MixedModel,args...
   cb.mvz .+= (dt .* cb.mvzdot)
 
   ### compute the instantaneous return
-#  # observable bias
-#  rval::Float64 = cb.Afunc(system,mm,ind,dt)# + Bfunc(system,mm,ind,dt)
-
   # action difference - positive term
   cb.em .= ((system.thermostat.scale/sqrt(dt)) .* system.thermostat.rands).^2
   # action difference - negative term
   cb.em .-= (mm.potentials[length(mm.potentials)].f .+ (system.thermostat.scale/sqrt(dt)) .* system.thermostat.rands).^2
   # compute return
-#  rval += sum(cb.em) / (2*system.thermostat.scale^2)
   rval::Float64 = sum(cb.em) / (2*system.thermostat.scale^2)
 
   # add return to KL divergence
@@ -83,41 +79,3 @@ function callback(cb::MCVBCallback,system::AbstractSystem,mm::MixedModel,args...
   cb.gradf .+= (rval .* cb.mvy) .- ((vval/dt) .* (cb.mvydot*system.thermostat.rands))
   cb.gradv .+= (rval .* cb.mvz) .- (vval .* cb.mvzdot)
 end
-
-"""
-function callback(cb::MCVBCallback,system::AbstractSystem,mm::MixedModel,args...)
-  # extract some arguments
-  ind::Int64 = args[1]
-  dt::Float64 = args[2]
-
-  # compute gradient of variable force and update ydot
-  jacobian!(cb.mvydot,system,mm.potentials[length(mm.potentials)])
-  cb.mvydot .*= (sqrt(dt)/system.thermostat.scale)
-  cb.mvy .+= (cb.mvydot*ones(Float64,2))
-
-  # calculate value baseline function
-  vval::Float64 = callvbl(system,cb.vbl)
-
-  # compute gradient of value baseline
-  gradient!(cb.mvzdot,system,cb.vbl)
-  cb.mvz .+= (dt .* cb.mvzdot)
-
-  ### compute the instantaneous return
-  # observable bias
-  rval::Float64 = cb.Afunc(system,mm,ind,dt)# + Bfunc(system,mm,ind,dt)
-
-  # action difference - positive term
-  cb.em .= (system.thermostat.scale/sqrt(dt) .* ones(Float64,2)).^2
-  # action difference - negative term
-  cb.em .-= (mm.potentials[length(mm.potentials)].f .+ (system.thermostat.scale/sqrt(dt)) .* ones(Float64,2)).^2
-  # compute return
-  rval += sum(cb.em) / (2*system.thermostat.scale^2)
-
-  # add return to KL divergence
-  cb.dkl += rval*dt
-
-  # update the gradients of the force parameters and the value baseline parameters
-  cb.gradf .+= (rval .* cb.mvy) .- ((vval/dt) .* (cb.mvydot*ones(Float64,2)))
-  cb.gradv .+= (rval .* cb.mvz) .- (vval .* cb.mvzdot)
-end
-"""
