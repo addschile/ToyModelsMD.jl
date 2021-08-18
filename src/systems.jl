17b55fb670c1350651722e78fce0c21ea2c75a90
Addison Schile <addschile@gmail.com>
2021-08-17 21:22:00 -0700

    started adding overdamped system capabilities

--- src/systems.jl
+++ src/systems.jl
@@ -1,5 +1,8 @@
 struct DummySystem <: AbstractSystem; end
 
+"""
+Generic struct for a system
+"""
 mutable struct System <: AbstractSystem
   dim::Int64
   t::Float64
@@ -18,6 +21,7 @@ function System(model::AbstractPotential)
 end
 
 """
+Generic struct for a system with a thermostat
 """
 mutable struct ThermostattedSystem <: AbstractThermostattedSystem
   dim::Int64
@@ -37,6 +41,51 @@ function ThermostattedSystem(model::AbstractPotential,thermostat::AbstractThermo
   ThermostattedSystem(dim,0.0,x,f,thermostat)
 end
 
+"""
+Generic struct for a system with a thermostat
+"""
+mutable struct ThermostattedSystem <: AbstractThermostattedSystem
+  dim::Int64
+  t::Float64
+  x::Vector{Float64}
+  f::Vector{Float64}
+  thermostat::AbstractThermostat
+  function ThermostattedSystem(dim::Int64,t::Float64,x::Vector{Float64},f::Vector{Float64},thermostat::AbstractThermostat)
+    new(dim,t,x,f,thermostat)
+  end
+end
+
+function ThermostattedSystem(model::AbstractPotential,thermostat::AbstractThermostat)
+  dim::Int64 = getdimensionality(model)
+  x::Vector{Float64} = zeros(dim)
+  f::Vector{Float64} = zeros(dim)
+  ThermostattedSystem(dim,0.0,x,f,thermostat)
+end
+
+#"""
+#Struct for an overdamped system with a langevin thermostat
+#"""
+#mutable struct OverdampedLangevinSystem <: AbstractThermostattedSystem
+#  dim::Int64
+#  t::Float64
+#  x::Vector{Float64}
+#  f::Vector{Float64}
+#  thermostat::AbstractLangevin
+#  function OverdampedLangevinSystem(dim::Int64,t::Float64,x::Vector{Float64},f::Vector{Float64},thermostat::AbstractLangevin)
+#    new(dim,t,x,f,thermostat)
+#  end
+#end
+#
+#function OverdampedLangevinSystem(model::AbstractPotential,thermostat::AbstractLangevin)
+#  dim::Int64 = getdimensionality(model)
+#  x::Vector{Float64} = zeros(dim)
+#  f::Vector{Float64} = zeros(dim)
+#  ThermostattedSystem(dim,0.0,x,f,thermostat)
+#end
+
+"""
+Functions for interacting with the force
+"""
 function zeroforce!(system::AbstractSystem)
   system.f .= 0.0
 end
@@ -45,7 +94,18 @@ function addforce!(dt::Float64,system::AbstractSystem,model::AbstractPotential)
   system.x .+= dt .* model.f
 end
 
+# TODO maybe take this function out since it's a repeat of the one above
+function addforce!(dt::Float64,system::ThermostattedSystem,model::AbstractPotential)
+  system.x .+= dt .* model.f
+end
+
+# TODO do unit tests on Overdamped system before allowing this
+#function addforce!(dt::Float64,system::OverdampedLangevinSystem,model::AbstractPotential)
+#  system.x .+= dt .* model.f ./ system.thermostat.gamma
+#end
+
 """
+Struct for Active Brownian particle system
 """
 mutable struct ActiveBrownianSystem <: AbstractThermostattedSystem
   dim::Int64
@@ -66,34 +126,12 @@ function ActiveBrownianSystem(v0::Float64,model::AbstractPotential,thermostat::A
   ActiveBrownianSystem(dim+1,v0,0.0,x,f,thermostat)
 end
 
+# TODO idk yet, but this needs to be fixed to allow the different gammas
 function addforce!(dt::Float64,sys::ActiveBrownianSystem,model::AbstractPotential)
   if model.dim == sys.dim
-    sys.x += dt .* model.f
+    sys.x += dt .* model.f ./ system.thermostat.gamma
     @. @views sys.x[1:sys.dim-1] .+= (dt*sys.v0) .* [cos(sys.x[end]),sin(sys.x[end])]
   else
-    @. @views sys.x[1:sys.dim-1] .+= dt .* (model.f[1:sys.dim-1] + (sys.v0 .* [cos(sys.x[end]),sin(sys.x[end])]))
+    @. @views sys.x[1:sys.dim-1] .+= dt .* (model.f[1:sys.dim-1] ./ system.thermostat.gamma[1:sys.dim-1] + (sys.v0 .* [cos(sys.x[end]),sin(sys.x[end])]))
   end
 end
-
-#function fderivative(du::AbstractArray,u::AbstractArray,sys::AbstractSystem)
-#  force!(u,du,sys.em,sys.model)
-#end
-#
-#function gderivative(du::AbstractArray,u::AbstractArray,therm::AbstractThermostat)
-#  diffusion!(du,therm)
-#end
-#
-#mutable struct OverdampedSystem <: AbstractSystem
-#  model::AbstractPotential
-#  thermostat::AbstractThermostat
-#  x::Array{Float64}
-#  f::Array{Float64}
-#  em::AbstractArray
-#  function OverdampedSystem(model::AbstractPotential,thermostat::AbstractThermostat)
-#    dim::Int64 = getdimensionality(model)
-#    x::Array{Float64} = zeros(dim)
-#    f::Array{Float64} = zeros(dim)
-#    em::AbstractArray = genextramem(model)
-#    new(model,thermostat,x,f,em)
-#  end
-#end
