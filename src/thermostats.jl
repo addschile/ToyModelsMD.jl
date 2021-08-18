8fbcf536de75da94c518e4362cf164f89e88e03a
Addison Schile <addschile@gmail.com>
2021-08-17 20:47:56 -0700

    added seed and rng access functions

--- src/thermostats.jl
+++ src/thermostats.jl
@@ -1,18 +1,23 @@
 struct DummyThermostat <: AbstractThermostat; end
 
+"""
+Generic Langevin thermostat for a single particle
+"""
 mutable struct Langevin <: AbstractLangevin
   T::Float64
   gamma::Float64
   scale::Float64
+  seed::UInt64
   rng::AbstractRNG
   rands::Vector{Float64}
   function Langevin(T::Float64,gamma::Float64,seed::UInt64)
-    new(T,gamma,sqrt(2.0*T/gamma),MersenneTwister(seed),[])
+    new(T,gamma,sqrt(2.0*T/gamma),seed,MersenneTwister(seed),[])
   end
 end
 Langevin(T::Float64,gamma::Float64) = Langevin(T,gamma,rand(UInt64))
 
 """
+Langevin thermostat for a single active particle
 """
 mutable struct ActiveLangevin <: AbstractLangevin
   Tt::Float64
@@ -20,18 +25,32 @@ mutable struct ActiveLangevin <: AbstractLangevin
   Tr::Float64
   gammar::Float64
   scale::Vector{Float64}
+  seed::UInt64
   rng::AbstractRNG
   rands::Vector{Float64}
   function ActiveLangevin(dim::Int64,Tt::Float64,gammat::Float64,Tr::Float64,gammar::Float64,seed::UInt64)
     scale = ones(Float64,dim+1)
     scale[1:dim] .= sqrt(2.0*Tt/gammat)
     scale[end] = sqrt(2.0*Tr/gammar)
-    new(Tt,gammat,Tr,gammar,scale,MersenneTwister(seed),[])
+    new(Tt,gammat,Tr,gammar,scale,seed,MersenneTwister(seed),[])
   end
 end
 ActiveLangevin(dim::Int64,Tt::Float64,gammat::Float64,Tr::Float64,gammar::Float64) = ActiveLangevin(dim,Tt,gammat,Tr,gammar,rand(UInt64))
 
 """
+Functions for access to the random number generator
+"""
+function getseed(langevin::AbstractLangevin)
+  return langevin.seed
+end
+
+function setseed!(seed::Union{Int64,UInt64},langevin::AbstractLangevin)
+  langevin.seed = convert(UInt64,seed)
+  langevin.rng = MersenneTwister(langevin.seed)
+end
+
+"""
+Various functions for generating noise
 """
 function gennoises(nsteps::Int64,dim::Int64,rng::AbstractRNG)
   randn(rng,Float64,(nsteps,dim))
@@ -43,7 +62,6 @@ end
 
 function gennoise!(dim::Int64,langevin::AbstractLangevin)
   langevin.rands = randn(langevin.rng,Float64,dim)
-  #langevin.rands = ones(Float64,dim)
 end
 
 """
