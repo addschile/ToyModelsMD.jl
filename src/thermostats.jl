3833cdfaeb096acd9ca4ace59e532c25d9d245e0
Addison Schile <addschile@gmail.com>
2021-08-19 11:28:27 -0700

    cleaned up code from unnecessary comments and returns

--- src/thermostats.jl
+++ src/thermostats.jl
@@ -41,8 +41,6 @@ function applythermostat!(dt::Float64,sys::AbstractThermostattedSystem,langevin:
   sys.x .+= (sqrt(dt) .* langevin.scale .* langevin.rands)
 end
 
-
-
 """
 Langevin thermostat for a single active particle
 """
@@ -64,7 +62,6 @@ mutable struct ActiveLangevin <: AbstractLangevin
 end
 ActiveLangevin(dim::Int64,Tt::Float64,gammat::Float64,Tr::Float64,gammar::Float64) = ActiveLangevin(dim,Tt,gammat,Tr,gammar,rand(UInt64))
 
-# TODO need to make this work
 function applythermostat!(dt::Float64,sys::AbstractActiveSystem,langevin::ActiveLangevin)
   @. @views sys.x[1:sys.dim-1] .+= (dt*sys.v0) .* [cos(sys.x[end]),sin(sys.x[end])]
   sys.x .+= (sqrt(dt) .* langevin.scale .* langevin.rands)
