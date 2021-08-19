8fc1d39b4cae5c7c1f263f8f8e9c478e8c88e23b
Addison Schile <addschile@gmail.com>
2021-08-19 08:59:46 -0700

    added functionality to have mixed callbacks similar to mixed models

---
+++ src/mixedcallback.jl
@@ -0,0 +1,14 @@
+"""
+Mixed callback struct for handling multiple callback functions in a single run
+"""
+mutable struct MixedCallback <: AbstractCallback
+    cbs::Vector{T} where T <: AbstractCallback
+    function MixedCallback(cbs::Vector{T}) where T <: AbstractCallback
+        new(cbs)
+    end
+
+function callback(cbs::MixedCallback,system::AbstractSystem,model::AbstractPotential,args...)
+    for cb in cbs
+        callback(cb,system,model,args)
+    end
+end
