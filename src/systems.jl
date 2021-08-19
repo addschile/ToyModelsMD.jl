11ebc5d726b7a4dad19b647ff498c51d2eb4452a
Addison Schile <addschile@gmail.com>
2021-08-19 11:26:47 -0700

    moved active system to separate file

--- src/systems.jl
+++ src/systems.jl
@@ -104,35 +104,3 @@ end
 #function addforce!(dt::Float64,system::OverdampedLangevinSystem,model::AbstractPotential)
 #  system.x .+= dt .* model.f ./ system.thermostat.gamma
 #end
-
-"""
-Struct for Active Brownian particle system
-"""
-mutable struct ActiveBrownianSystem <: AbstractThermostattedSystem
-  dim::Int64
-  v0::Float64
-  t::Float64
-  x::Vector{Float64}
-  f::Vector{Float64}
-  thermostat::ActiveLangevin
-  function ActiveBrownianSystem(dim::Int64,v0::Float64,t::Float64,x::Vector{Float64},f::Vector{Float64},thermostat::ActiveLangevin)
-    new(dim,v0,t,x,f,thermostat)
-  end
-end
-
-function ActiveBrownianSystem(v0::Float64,model::AbstractPotential,thermostat::ActiveLangevin)
-  dim::Int64 = getdimensionality(model)
-  x::Vector{Float64} = zeros(dim+1)
-  f::Vector{Float64} = zeros(dim)
-  ActiveBrownianSystem(dim+1,v0,0.0,x,f,thermostat)
-end
-
-# TODO idk yet, but this needs to be fixed to allow the different gammas
-function addforce!(dt::Float64,sys::ActiveBrownianSystem,model::AbstractSinglePotential)
-  if model.dim == sys.dim
-    sys.x += dt .* model.f ./ system.thermostat.gamma
-#    @. @views sys.x[1:sys.dim-1] .+= (dt*sys.v0) .* [cos(sys.x[end]),sin(sys.x[end])]
-  else
-    @. @views sys.x[1:sys.dim-1] .+= dt .* (model.f[1:sys.dim-1] ./ system.thermostat.gamma[1:sys.dim-1])
-  end
-end
